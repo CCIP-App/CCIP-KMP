f6ffb1f2d8dd89c9c20a842c3be31338fc72125a
Brian Chang <viere0928@gmail.com>
2024-08-18 22:41:50 +0800

    Initial iOS project

---
+++ iosApp/OPass/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OPass
+//
+//  Created by Brian Chang on 2024/8/18.
+//  2024 OPass.
+//
+
+import SwiftUI
+import shared
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text(Greeting().greet())
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iosApp/OPass/OPassApp.swift
@@ -0,0 +1,18 @@
+//
+//  OPassApp.swift
+//  OPass
+//
+//  Created by Brian Chang on 2024/8/18.
+//  2024 OPass.
+//
+
+import SwiftUI
+
+@main
+struct OPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
