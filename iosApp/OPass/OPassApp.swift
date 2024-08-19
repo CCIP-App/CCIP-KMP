c4800468369e92bc255f41daf8726a330607d885
Brian Chang <viere0928@gmail.com>
2024-08-20 00:13:47 +0800

    Setup OneSignal

--- iosApp/OPass/OPassApp.swift
+++ iosApp/OPass/OPassApp.swift
@@ -6,13 +6,30 @@
 //  2024 OPass.
 //
 
+import OSLog
 import SwiftUI
+import OneSignalFramework
 
 @main
 struct OPassApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    private let logger = Logger(subsystem: "OPassApp", category: "AppDelegate")
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // MARK: - Configure OneSignal
+        OneSignal.initialize("b6213f49-e356-4b48-aa9d-7cf10ce1904d", withLaunchOptions: launchOptions)
+        OneSignal.Notifications.requestPermission({ accepted in
+            self.logger.info("User accepted notifications: \(accepted)")
+        }, fallbackToSettings: false)
+       return true
     }
 }
