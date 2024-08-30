05213c4703baf2c84df74206697def305d3af6b1
Aayush Gupta <aayushgupta219@gmail.com>
2024-08-30 19:50:54 +0530

    androidApp: EventPreviewScreen: Add search bar support
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- androidApp/src/main/java/app/opass/ccip/android/ui/screens/eventpreview/EventPreviewScreen.kt
+++ androidApp/src/main/java/app/opass/ccip/android/ui/screens/eventpreview/EventPreviewScreen.kt
@@ -8,6 +8,7 @@ package app.opass.ccip.android.ui.screens.eventpreview
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,21 +18,31 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,7 +51,6 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import app.opass.ccip.android.MainViewModel
 import app.opass.ccip.android.R
-import app.opass.ccip.android.ui.components.TopAppBar
 import app.opass.ccip.android.ui.extensions.sharedPreferences
 import app.opass.ccip.android.ui.extensions.shimmer
 import app.opass.ccip.android.ui.navigation.Screen
@@ -55,15 +65,76 @@ fun Screen.EventPreview.EventPreviewScreen(
     navHostController: NavHostController,
     viewModel: MainViewModel
 ) {
+    var query by rememberSaveable { mutableStateOf("") }
+    var isExpanded by rememberSaveable { mutableStateOf(false) }
+
+    val sharedPreferences = LocalContext.current.sharedPreferences
+    val isRefreshing by viewModel.isRefreshing.collectAsStateWithLifecycle()
+    val events by viewModel.events.collectAsStateWithLifecycle()
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-        topBar = { TopAppBar(title = stringResource(id = this.title)) }
+        topBar = {
+            Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                SearchBar(
+                    inputField = {
+                        SearchBarDefaults.InputField(
+                            query = query,
+                            onQueryChange = { query = it },
+                            onSearch = { query = it },
+                            expanded = isExpanded,
+                            onExpandedChange = { isExpanded = it; query = String() },
+                            enabled = !events.isNullOrEmpty(),
+                            leadingIcon = {
+                                if (isExpanded) {
+                                    IconButton(onClick = { isExpanded = false; query = String() }) {
+                                        Icon(
+                                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                            contentDescription = null
+                                        )
+                                    }
+                                } else {
+                                    Icon(Icons.Default.Search, contentDescription = null)
+                                }
+                            },
+                            trailingIcon = {
+                                if (isExpanded) {
+                                    IconButton(onClick = { query = String() }) {
+                                        Icon(
+                                            imageVector = Icons.Filled.Clear,
+                                            contentDescription = null
+                                        )
+                                    }
+                                } else {
+                                    IconButton(onClick = { viewModel.getEvents(true) }) {
+                                        Icon(
+                                            painter = painterResource(id = R.drawable.ic_refresh),
+                                            contentDescription = null
+                                        )
+                                    }
+                                }
+                            },
+                            placeholder = { Text(text = stringResource(id = R.string.search_event)) }
+                        )
+                    },
+                    expanded = isExpanded,
+                    onExpandedChange = { isExpanded = it; query = String() }
+                ) {
+                    LazyColumn {
+                        items(
+                            items = events!!.filter { it.name.contains(query, true) },
+                            key = { e -> e.id }
+                        ) { event: Event ->
+                            EventPreviewItem(name = event.name, logoUrl = event.logoUrl) {
+                                sharedPreferences.edit { putString(CURRENT_EVENT_ID, event.id) }
+                                navHostController.navigate(Screen.Event(event.id))
+                            }
+                        }
+                    }
+                }
+            }
+        }
     ) { paddingValues ->
-
-        val sharedPreferences = LocalContext.current.sharedPreferences
-        val isRefreshing by viewModel.isRefreshing.collectAsStateWithLifecycle()
-        val events by viewModel.events.collectAsStateWithLifecycle()
-
         PullToRefreshBox(
             isRefreshing = isRefreshing,
             onRefresh = { viewModel.getEvents(true) },
