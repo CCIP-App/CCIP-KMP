292f4013d4e6a39b52fee9c8c8858f9eb83b25d1
Aayush Gupta <aayushgupta219@gmail.com>
2025-04-10 16:03:09 +0800

    androidApp: SessionScreen: Move menu buttons to drop down menu
    Mirror iOS UI
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

---
+++ androidApp/src/main/java/app/opass/ccip/android/ui/menu/item/SessionMenuItem.kt
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2025 OPass
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+package app.opass.ccip.android.ui.menu.item
+
+/**
+ * Valid menu items for session screen menu
+ */
+enum class SessionMenuItem {
+    ADD_TO_CALENDAR,
+    SHARE
+}
