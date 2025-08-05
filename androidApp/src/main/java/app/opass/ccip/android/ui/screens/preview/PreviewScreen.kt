8f62226161b06ffc6ca7b91a0a25280354f55f09
Aayush Gupta <aayushgupta219@gmail.com>
2025-08-05 21:10:48 +0800

    androidApp: Use the new M3Expressive loading indicator for pull to refresh
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- androidApp/src/main/java/app/opass/ccip/android/ui/screens/preview/PreviewScreen.kt
+++ androidApp/src/main/java/app/opass/ccip/android/ui/screens/preview/PreviewScreen.kt
@@ -11,10 +11,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,7 +53,7 @@ fun PreviewScreen(
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 private fun ScreenContent(
     events: List<Event> = emptyList(),
@@ -61,6 +65,7 @@ private fun ScreenContent(
     onNavigateToEvent: (eventId: String) -> Unit = {},
 ) {
     val sharedPreferences = LocalContext.current.sharedPreferences
+    val state = rememberPullToRefreshState()
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -85,7 +90,15 @@ private fun ScreenContent(
         PullToRefreshBox(
             isRefreshing = isRefreshing,
             onRefresh = onRefresh,
-            modifier = Modifier.padding(paddingValues)
+            modifier = Modifier.padding(paddingValues),
+            state = state,
+            indicator = {
+                PullToRefreshDefaults.LoadingIndicator(
+                    state = state,
+                    isRefreshing = isRefreshing,
+                    modifier = Modifier.align(Alignment.TopCenter)
+                )
+            }
         ) {
             LazyColumn(contentPadding = PaddingValues(horizontal = 20.dp)) {
                 if (events.isEmpty()) {
